b3dacdfc97867a2663802f437ff189d537fda271
carolyn-brodie <48027983+carolyn-brodie@users.noreply.github.com>
2023-01-25 19:54:55 -0600

    Updated

---
+++ src/School/HelloWorlld.java
@@ -0,0 +1,8 @@
+package School;
+
+public interface HelloWorlld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("Again");
+    }
+}
